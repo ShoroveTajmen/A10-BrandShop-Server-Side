82bf26a5ddca0e49032f9bd16f32033328600ff2
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-19 03:37:25 +0600

    PUT method added for updating product data and send to the database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 const corsOptions = {
@@ -15,11 +15,8 @@ app.use(cors(corsOptions));
 //access post body and convert into json format
 app.use(express.json());
 
-
 //connect to the mongodb
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es62grd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,32 +29,56 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     //create a database and database collection
-    const productCollection = client.db('productDB').collection('products');
+    const productCollection = client.db("productDB").collection("products");
 
     //using get method to read the data what i stored in database
-    app.get('/product', async(req, res)=>{
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/product", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //get method for getting specific product id / update specific product data
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     //using post method to store product in the database
-    app.post('/product', async(req, res) => {
-        const newProduct = req.body;
-        console.log(newProduct);
-        const result = await productCollection.insertOne(newProduct);
-        res.send(result);
-    })
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
-
-
-
-
-
-
-
+    //using put method to update the product data
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+          brand: updatedProduct.brand,
+          productName: updatedProduct.productName,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          productPic: updatedProduct.productPic,
+          rating: updatedProduct.rating,
+          description: updatedProduct.description,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
@@ -68,9 +89,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
 
 //for testing
 app.get("/", (req, res) => {
